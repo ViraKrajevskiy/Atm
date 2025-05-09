b4718c9d83a148bae821674223db502ccb633649
Vira <guidevirgate@gmail.com>
2025-05-09 16:19:48 +0500

    ul

---
+++ backend/classes/Card_and_money.py
@@ -0,0 +1,10 @@
+class CardAndMoney:
+    def __init__(self,card_id , card_name, card_number, card_work_time, password, balance, card_type, phone_number):
+        self.card_id = card_id
+        self.card_name = card_name
+        self.card_number = card_number
+        self.card_work_time = card_work_time
+        self.password = password
+        self.balance = balance
+        self.card_type = card_type
+        self.phone_number = phone_number
